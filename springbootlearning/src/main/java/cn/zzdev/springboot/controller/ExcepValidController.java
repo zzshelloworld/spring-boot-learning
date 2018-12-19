547cd81e97d480d2f8190b0d9fd683454111262a
zhangzhe <zhangzhe2324@gmail.com>
2018-12-20 00:42:50 +0800

    mybatis

---
+++ springbootlearning/src/main/java/cn/zzdev/springboot/controller/ExcepValidController.java
@@ -0,0 +1,22 @@
+package cn.zzdev.springboot.controller;
+
+import cn.zzdev.springboot.exception.CommonException;
+import cn.zzdev.springboot.req.DemoReq;
+import cn.zzdev.springboot.util.ValidatorUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.validation.Valid;
+
+public class ExcepValidController {
+    @GetMapping("/demo")
+    public String demo() {
+        throw new CommonException("01001", "发送异常");
+    }
+
+    @GetMapping("/demo/valid")
+    public String demoValid(@Valid DemoReq req) {
+        //手动校验
+        ValidatorUtil.validate(req);
+        return req.getCode() + "," + req.getName();
+    }
+}
